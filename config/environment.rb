c4c65f68859205de71e24b48b4bd25f8dbfee198
Daniel Dawson <dannyldawson@gmail.com>
2019-01-06 09:22:54 -0800

    add environment config

---
+++ config/environment.rb
@@ -0,0 +1,5 @@
+require "open-uri"
+
+require_relative "../lib/intermat_scrape/scraper.rb"
+require_relative "../lib/intermat_scrape/version.rb"
+require_relative "../lib/intermat_scrape/wrestler.rb"
