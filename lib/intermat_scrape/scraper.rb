b8ab3fd2bd2629802968b83a8d0f66358442cf45
Daniel Dawson <dannyldawson@gmail.com>
2019-01-08 10:45:06 -0800

    implement methods

--- lib/intermat_scrape/scraper.rb
+++ lib/intermat_scrape/scraper.rb
@@ -1,12 +1,26 @@
 class IntermatScrape::Scraper
 
-  BASE_URL = "https://www.intermatwrestle.com/rankings/college"
+  attr_accessor :weight
 
-  def get_doc
-    Nokogiri::HTML(open(BASE_URL))
+  BASE_URL = "https://www.intermatwrestle.com/rankings/college/"
+
+  def initialize(weight_class)
+    @weight_class = weight_class
   end
 
-  def get_wrestlers
-    get_doc.css("table.table tbody tr")
+  def self.get_doc_by_weight(weight_class)
+    new(weight_class).Nokogiri::HTML(open(BASE_URL + @weight_class.weight.to_s))
+  end
+
+  def get_rows(weight_class)
+    self.class.get_doc_by_weight(weight_class).css("table.table tbody tr")
+  end
+
+  def create_wrestlers(weight_class)
+    get_rows(weight_class).collect do |row|
+      IntermatScrape::Wrestler.new_by_row(row).tap do |w|
+        w.weight_class = weight_class
+      end
+    end
   end
 end
