f947a8986ed417b2628c4913d39378f3dbb239a8
Daniel Dawson <dannyldawson@gmail.com>
2019-01-08 19:49:04 -0800

    add methods

--- lib/intermat_scrape/wrestler.rb
+++ lib/intermat_scrape/wrestler.rb
@@ -1,3 +1,33 @@
 class IntermatScrape::Wrestler
 
-end 
+  attr_accessor :rank, :weight_class, :school, :class_standing, :conference
+  attr_reader :name
+
+  def initialize(rank, name, school, class_standing, conference)
+    @rank = rank
+    @name = name
+    @school = school
+    @class_standing = class_standing
+    @conference = conference
+  end
+
+  def self.new_by_row(row)
+    new(
+      row.children[1].text,
+      row.css(":nth-child(2) a").text,
+      row.css(":nth-child(3)").text,
+      row.css(":nth-child(4)").text,
+      row.css(":nth-child(5)").text
+    )
+  end
+
+  def to_s
+    puts ""
+    puts "----------- #{self.name} - #{self.rank} -----------"
+    puts ""
+    puts "School:           #{self.school}"
+    puts "Class:          #{self.class_standing}"
+    puts "Conference:      #{self.conference}"
+  end
+
+end
