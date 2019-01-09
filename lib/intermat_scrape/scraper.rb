440100fea96538fcd1bce0a1ae495a0b7e673024
Daniel Dawson <dannyldawson@gmail.com>
2019-01-08 19:50:51 -0800

    remove #initiate

--- lib/intermat_scrape/scraper.rb
+++ lib/intermat_scrape/scraper.rb
@@ -1,25 +1,21 @@
 class IntermatScrape::Scraper
 
-  attr_accessor :weight
+  attr_accessor :weight_class
 
   BASE_URL = "https://www.intermatwrestle.com/rankings/college/"
 
-  def initialize(weight_class)
-    @weight_class = weight_class
-  end
-
   def self.get_doc_by_weight(weight_class)
-    new(weight_class).Nokogiri::HTML(open(BASE_URL + @weight_class.weight.to_s))
+    Nokogiri::HTML(open(BASE_URL + weight_class.weight))
   end
 
-  def get_rows(weight_class)
-    self.class.get_doc_by_weight(weight_class).css("table.table tbody tr")
+  def self.get_rows(weight_class)
+    get_doc_by_weight(weight_class).css("table.table tbody tr")
   end
 
-  def create_wrestlers(weight_class)
+  def self.create_wrestlers(weight_class)
     get_rows(weight_class).collect do |row|
       IntermatScrape::Wrestler.new_by_row(row).tap do |w|
-        w.weight_class = weight_class
+        weight_class.add_wrestler(w)
       end
     end
   end
