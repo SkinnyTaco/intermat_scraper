a26aefeb25556eeb5a0a9d97c4c8fd43dd9c7657
Daniel Dawson <dannyldawson@gmail.com>
2019-01-08 19:52:52 -0800

    add methods

---
+++ lib/intermat_scrape/weight_class.rb
@@ -0,0 +1,48 @@
+class IntermatScrape::WeightClass
+
+  attr_accessor :wrestlers, :weight
+
+  WEIGHT_CLASSES = ["125","133","141","149","157","165","174","184","197","Hwt"]
+
+  @@all = []
+
+  def initialize(weight)
+    @weight = weight
+    @wrestlers = []
+    @@all << self
+  end
+
+    def self.create_weight_classes
+      WEIGHT_CLASSES.each do |weight|
+        new(weight)
+      end
+      @@all
+    end
+
+  def self.print_weights
+    puts WEIGHT_CLASSES
+  end
+
+  def self.find_by_weight(weight)
+    @@all.detect { |w| w.weight == weight}
+  end
+
+  def add_wrestler(wrestler)
+    @wrestlers << wrestler
+    wrestler.weight_class = self
+  end
+
+  #TODO move to Wrestler class
+  def print_wrestlers
+    @wrestlers.each.with_index { |w, i| puts "#{w.rank}\t\t#{w.name}"}
+  end
+
+  def get_wrestler_by_rank(rank)
+    @wrestlers.detect { |w| w.rank == rank }
+  end
+
+  def self.all
+    @@all
+  end
+
+end
